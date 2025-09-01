95154616bb49347d292ca5e87feffe7794acc5e1
fwz-blk <fawaz1.mohammed2@gmail.com>
2025-09-01 22:59:42 +0530

    added new file

---
+++ user_pass_progr.py
@@ -0,0 +1,9 @@
+username = 'Mary'
+password = 'swordfish'
+
+if username == 'Mary':          # ❶
+    print('Hello, Mary')
+    if password == 'swordfish': # ❷
+        print('Access granted.')
+    else:                       # ❸
+        print('Wrong password.')
