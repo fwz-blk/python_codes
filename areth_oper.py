eeebe045da4fd6f0768d322b62d95ceed7c83b99
fwz-blk <fawaz1.mohammed2@gmail.com>
2025-09-04 13:12:49 +0530

    changed some code

---
+++ areth_oper.py
@@ -0,0 +1,8 @@
+x=6
+y=4
+
+sum=x+y
+diff=x-y
+prod=x*y
+quo=6/4
+print(f'the numbers are x={x} & y={y} and their sum={sum} , difference={diff},product={prod},quotient={quo}')
--- decl_var.py
+++ decl_var.py
@@ -3,3 +3,5 @@ y='string'
 z=0.10
 m=True
 n=False
+
+print(f'the int x = {x}  / the string y ={y} / the float z={z} / the true boolean m ={m} / the false boolean n={n}')
