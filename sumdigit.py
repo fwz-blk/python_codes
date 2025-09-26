22047b347985454dd9054c2b25fdec2347a81364
fwz-blk <fawaz1.mohammed2@gmail.com>
2025-09-27 00:00:33 +0530

    added some changes

--- sumdigit.py
+++ sumdigit.py
@@ -5,9 +5,10 @@ while x>0:
     d=x%10
     x=x//10
     if d % 2 == 0:
-        osum=osum+d
-    elif d % 2 != 0:
         esum=esum+d
+    elif d % 2 != 0:
+        osum=osum+d
+        
 
 
 print(f"the sum of the even digits of the number {y} is {esum}")
